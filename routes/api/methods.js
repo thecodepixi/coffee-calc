9efd73efc68be0a2e1dd4e77a27182867157ac03
Emily <emily.a.harber@gmail.com>
2020-05-09 13:50:53 -0400

    api root route working!

--- routes/api/methods.js
+++ routes/api/methods.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const BrewMethods = require('../../brew-methods');
+
+//get all info for all methods
+router.get('/', (req, res) => {
+  res.json(BrewMethods);
+});
+
+module.exports = router;
