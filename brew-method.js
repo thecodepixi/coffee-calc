45afe3fea8ab6216efcf9176ddafe7f2da9ee3b9
Emily <emily.a.harber@gmail.com>
2020-05-09 13:23:55 -0400

    inital commit

---
+++ brew-method.js
@@ -0,0 +1,11 @@
+//much of this brewing info is sourced from Stumptown, Blue Bottle, and La Colombe
+//measurements are in grams! 1oz of water  = 29.6g
+
+const BrewMethods = [
+  {
+    name: 'French Press',
+    ratio: '',
+    guide: '',
+    tips: [],
+  },
+];
