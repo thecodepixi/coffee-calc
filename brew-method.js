33277ccfbe7100969a5acab85661c058edb6ea40
Emily <emily.a.harber@gmail.com>
2020-05-09 13:38:47 -0400

    add a few brew methods to model"

--- brew-method.js
+++ brew-method.js
@@ -1,10 +1,37 @@
-//much of this brewing info is sourced from Stumptown, Blue Bottle, and La Colombe
-//measurements are in grams! 1oz of water  = 29.6g
+// much of this brewing info is sourced from Stumptown, Blue Bottle, and La Colombe
+// measurements are in grams! 1oz of water  = 29.6g
+// ratios are a 1:n ratio of coffee to water.
+// ratio usage example: if you want to make a french press of 16oz of coffee you need 16*29.6 / 15 grams of coffee or a 1:15 ratio of coffee to water in grams.
+// should guide info be written in MD for rendering?
 
 const BrewMethods = [
   {
     name: 'French Press',
-    ratio: '',
+    ratio: 15,
+    guide: '',
+    tips: [],
+  },
+  {
+    name: 'Chemex',
+    ratio: 16,
+    guide: '',
+    tips: [],
+  },
+  {
+    name: 'Pour Over',
+    ratio: 17,
+    guide: '',
+    tips: [],
+  },
+  {
+    name: 'Moka Pot',
+    ratio: 7,
+    guide: '',
+    tips: [],
+  },
+  {
+    name: 'Aeropress',
+    ratio: 12,
     guide: '',
     tips: [],
   },
